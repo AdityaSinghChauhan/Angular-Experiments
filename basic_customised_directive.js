4695c5bcdf1f6df2e0f94c24110010edc00efe8a
Aditya Singh Chauhan <astroadityachauhan@gmail.com>
2016-07-12 17:21:13 +0530

    Create basic_customised_directive.js
    This is the way you can use your own directive rather than using conventional angular's.

---
+++ basic_customised_directive.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body>
+
+<div data-ng-app="" data-ng-init="firstName='Aditya'">
+
+<p>The name is <span data-ng-bind="firstName"></span></p>
+
+</div>
+
+</body>
+</html>
