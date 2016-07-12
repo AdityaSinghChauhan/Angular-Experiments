4a2f7a7fa75a1ec92fd50cc45cf563fb1227a926
Aditya Singh Chauhan <astroadityachauhan@gmail.com>
2016-07-12 17:18:30 +0530

    Create copy_name.js
    A basic fun application, which echoes the content of primary text box to another text box.

---
+++ copy_name.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body>
+
+<div ng-app="">
+ 
+<p>Input something in the input box:</p>
+<p>Name : <input type="text" ng-model="name"></p>
+<p>Copy Name : <input type="text" value="{{name}}">
+
+</div>
+
+</body>
+</html>
