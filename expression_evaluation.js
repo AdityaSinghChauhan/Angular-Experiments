6864213188d11b22f03eb246f58e51c23727b3a8
AdityaSinghChauhan <astroadityachauhan@gmail.com>
2016-07-12 17:36:16 +0530

    expression_evaluation.js file added

---
+++ expression_evaluation.js
@@ -0,0 +1,13 @@
+CTYPE html>
+<html>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body>
+
+<div ng-app="">
+<p>Calculated Expression: {{ 3 + 7 }}</p>
+</div>
+
+</body>
+</html>
+
+
